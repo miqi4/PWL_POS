39fe883ac15df2d184bd538a527d0decc83258e5
Mohammad iqbal <iqbalop098@gmail.com>
2026-02-28 11:53:56 +0700

    Praktikum-2.2

---
+++ database/migrations/2026_02_28_044837_create_t_stok_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+    $table->id('stok_id');
+    $table->unsignedBigInteger('barang_id');
+    $table->integer('stok_jumlah');
+    $table->date('stok_tanggal');
+    $table->timestamps();
+
+    $table->foreign('barang_id')
+          ->references('barang_id')
+          ->on('m_barang');
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_stok');
+    }
+};
